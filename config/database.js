63eb6af6faa50e52a00430c6ee47e475ab110c95
shanemcdaid96 <L00117529@student.lyit.ie>
2018-11-25 12:58:48 +0000

    First commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+   // remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    localUrl: 'mongodb://localhost:27017/mean',
+
+    cosmosdb:{
+    	     url:'mongodb://shanemcdaid:Zvg1SdeUfxOb9zT3KjvV12e3e99Fdn2JjQqNpxsp1PfdImkUEb3Slh6GNrdsxX1TrtgPpeatH9re0eT09lVzKQ==@shanemcdaid.documents.azure.com:10255/?ssl=true',
+    	     useMongoClient: true
+    }
+};
